9eb72dc82a000503e6bde28f8b66b6f794cec863
mianharoon1197 <harooniftikhar699@gmail.com>
2025-08-04 20:39:10 +0500

    Updated Size of bottom curve

--- SignUpForm/components/BottomCurve/BottomCurve.tsx
+++ SignUpForm/components/BottomCurve/BottomCurve.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Svg, { Path, Defs, LinearGradient, Stop } from 'react-native-svg';
 
 const BottomCurve = () => {
   return (
     <View style={styles.curve}>
-      <Svg width="100%" height={190} viewBox="0 0 600 120">
+      <Svg width={300} height={250} viewBox="0 0 300 120">
         <Defs>
-          <LinearGradient id="grad" x1="0.8" y1="0" x2="0.9" y2="0.8">
+          <LinearGradient id="grad" x1="0.2" y1="0" x2="1" y2="1">
             <Stop offset="0%" stopColor="#7F00FF" />
+            <Stop offset="25%" stopColor="#7F00FF" />
             <Stop offset="100%" stopColor="#007BFF" />
           </LinearGradient>
         </Defs>
@@ -16,9 +16,10 @@ const BottomCurve = () => {
         <Path
           d={`
             M 0 50 
-            Q 60 32 130 55
-            Q 240 95 258 200 
-            L 0 200 Z
+            Q 80 28 147 60
+            Q 248 115 257 200 
+            L 0 200 
+            Z
           `}
           fill="url(#grad)"
         />
@@ -32,7 +33,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     left: 0,
-    width:"100%",
   },
 });
 
