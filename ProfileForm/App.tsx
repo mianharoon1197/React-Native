be9d1e7db30ba1f0040f4338aa163ee462f715f6
mianharoon1197 <harooniftikhar699@gmail.com>
2025-08-09 13:06:52 +0500

    updated App

--- ProfileForm/App.tsx
+++ ProfileForm/App.tsx
@@ -3,23 +3,36 @@ import {
   StyleSheet,
   useColorScheme,
   View,
-  Text,
+  ScrollView,
+  SafeAreaView,
 } from 'react-native';
+
 import TopCurve from './components/TopCurve/TopCurve';
 import MainContent from './components/MainContent/MainContent';
-
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>Hello Haroon!!</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar barStyle={isDarkMode ? 'dark-content' : 'light-content'} />
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <View style={styles.container}>
+          <TopCurve />
+          <MainContent />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  scrollContent: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
   },
