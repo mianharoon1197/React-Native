74d80026fea344c1d27873598bfef34a6588e61e
mianharoon1197 <harooniftikhar699@gmail.com>
2025-08-13 11:38:36 +0500

    Updated App

--- chatForm/App.tsx
+++ chatForm/App.tsx
@@ -1,19 +1,21 @@
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
+import ChatScreen from './components/ChatScreen/ChatScreen';
+import Test from './components/Test/Test';
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <StatusBar barStyle={isDarkMode ? 'dark-content' : 'light-content'} />
+      <ChatScreen/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'rgba(62, 6, 206, 1)',
+    //paddingTop: 200,
     flex: 1,
   },
 });
