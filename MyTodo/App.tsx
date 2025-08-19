d392d6b2c5ccc9f020d8baffe225cdab516caadd
mianharoon1197 <harooniftikhar699@gmail.com>
2025-08-19 12:44:29 +0500

    Added Aysnc Storage

--- MyTodo/App.tsx
+++ MyTodo/App.tsx
@@ -2,18 +2,20 @@ import {
   StatusBar,
   StyleSheet,
   useColorScheme,
-  Text,
   View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { useEffect, useState } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { useState } from 'react';
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -27,13 +29,47 @@ function App() {
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
+  const [todo, setTodos] = useState<
+    { id: string; title: string; count: number }[]
+  >([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  const [todo, setTodos] = useState<{ id: string; title: string }[]>([]);
+  //load todos
+  useEffect(() => {
+    const loadTodos = async () => {
+      try {
+        const storedTodo = await AsyncStorage.getItem('myTodos');
+        if (storedTodo) {
+          setTodos(JSON.parse(storedTodo));
+        }
+      } catch (error) {
+        console.log('Unable to load todo: ', error);
+      } finally {
+        setIsLoaded(true);
+      }
+    };
+    loadTodos();
+  }, []);
+
+  //save todo
+  useEffect(() => {
+    if (!isLoaded) return;
+    const saveTodos = async () => {
+      try {
+        await AsyncStorage.setItem('myTodos', JSON.stringify(todo));
+      } catch (error) {
+        console.log('Unable to save todo: ', error);
+      }
+    };
+    saveTodos();
+  }, [todo]);
 
   const addTodo = (task: string) => {
-    console.log('Task Added: ', task);
     if (task.trim() === '') return;
-    setTodos(prev => [...prev, { id: Date.now().toString(), title: task }]);
+    setTodos(prev => [
+      ...prev,
+      { id: Date.now().toString(), title: task, count: 0 },
+    ]);
   };
 
   const deleteTodo = (id: string) => {
@@ -42,12 +78,34 @@ function AppContent() {
 
   const editTodo = (id: string, newText: string) => {
     setTodos(prev =>
-      prev.map(todo => 
-        todo.id === id ? { ...todo, title: newText } : todo),
+      prev.map(todo => (todo.id === id ? { ...todo, title: newText } : todo)),
     );
   };
 
+  const clearAllTodos = () => {
+    setTodos([]);
+  };
 
+  const incrementCount = (id: string) => {
+    setTodos(prev =>
+      prev.map(todo =>
+        todo.id === id ? { ...todo, count: todo.count + 1 } : todo,
+      ),
+    );
+  };
+  const decrementCount = (id: string) => {
+    setTodos(prev =>
+      prev.map(todo =>
+        todo.id === id ? { ...todo, count: todo.count - 1 } : todo,
+      ),
+    );
+  };
+
+  const clearCount = (id: string) => {
+    setTodos(prev =>
+      prev.map(todo => (todo.id === id ? { ...todo, count: 0 } : todo)),
+    );
+  };
   return (
     <View
       style={[
@@ -61,7 +119,16 @@ function AppContent() {
       ]}
     >
       <TodoInput addTodo={addTodo} />
-      <TodoList todos={todo} editTodo={editTodo} deleteTodo={deleteTodo} />
+
+      <TodoList
+        todos={todo}
+        editTodo={editTodo}
+        deleteTodo={deleteTodo}
+        clearAllTodos={clearAllTodos}
+        incrementCount={incrementCount}
+        decrementCount={decrementCount}
+        clearCount={clearCount}
+      />
     </View>
   );
 }
@@ -71,6 +138,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingHorizontal: 30,
+  },
+
+  buttonContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  clearButton: {
+    backgroundColor: '#f74e3f',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
